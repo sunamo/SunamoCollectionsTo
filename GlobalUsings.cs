1384d532b0d49e4394980bf5906e78d6df0e194c
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 14:21:52 +0200

    Before auto removing non public code elements

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,9 @@
 global using System.Linq;
 global using System.Collections.Generic;
-
+global using System;
+global using System.Text;
+global using System.Collections;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Reflection;
+global using System.Net;
+global using System.Diagnostics;
