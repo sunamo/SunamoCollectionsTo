fcd675c7dd2465e6cf0e96c30b92cf0b7cd73a23
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 12:28:24 +0200

    Removed unused file

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,12 +1,3 @@
 global using System.Linq;
 global using System.Collections.Generic;
-global using System;
-global using System.Text;
-global using System.Collections;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Reflection;
-global using System.Net;
-global using SunamoCollectionsTo;
-global using System.Diagnostics;
-global using SunamoCollectionsTo;
 
